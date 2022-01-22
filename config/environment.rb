3479ea39c8058857f5986a104e6f911ddfa709b2
koleshkos <koleshkoser@gmail.com>
2022-01-22 23:04:46 +0300

    added confimable to user with mailgun

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  port: 587,
+  address: 'smtp.mailgun.org',
+  user_name: ENV['MAILGUN_SMTP_LOGIN'],
+  password: ENV['MAILGUN_SMTP_PASSWORD'],
+  domain: 'heroku.com',
+  authentication: :plain,
+  enable_starttls_auto: true
+}
+ActionMailer::Base.delivery_method = :smtp
